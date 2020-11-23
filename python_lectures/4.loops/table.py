1fa16c5b80069911b93e61e0e18d3d8c62c6f18e
shubham <joshi.cse17@gmail.com>
2020-11-23 23:20:42 +0530

    adding loops 2

--- python_lectures/4.loops/table.py
+++ python_lectures/4.loops/table.py
@@ -1,24 +1,37 @@
-# user_input = int(input())
+# # user_input = int(input())
 
-# 2 X 1 = 2
-# 2 X 2 = 4
-# 2 X 3 = 6
-# 2 ... 
+# # 2 X 1 = 2
+# # 2 X 2 = 4
+# # 2 X 3 = 6
+# # 2 ... 
 
-# 3 X 1 = 3
-# 3 X 2 = 6
-
-
-for i in range(1, 21):
-    print("*****************")
-    print("Table of ", i)
-    for j in range(1, 11):
-        print(i, "X", j, "=", i * j)
-    print("*****************")
+# # 3 X 1 = 3
+# # 3 X 2 = 6
 
 
-sum = 0
-for j in range(2, 4):
-    for i in range(1, 3):
-        if i % 2 == 0:
-            sum += (i + j)
+# for i in range(1, 21):
+#     print("*****************")
+#     print("Table of ", i)
+#     for j in range(1, 11):
+#         print(i, "X", j, "=", i * j)
+#     print("*****************")
+
+
+# sum = 0
+# for j in range(2, 4):
+#     for i in range(1, 3):
+#         if i % 2 == 0:
+#             sum += (i + j)
+
+
+for i in range(1, 5):
+    for j in range(i):
+        print(i, end="")
+    print()
+
+
+
+# 1
+# 2 2 
+# 3 3 3
+# 4 4 4 4
---
+++ python_lectures/5. loops 2/intro.py
@@ -0,0 +1,98 @@
+"""
+For loop version 2.
+
+for ch in "ABCDEFGHIJ":
+    print(ch)
+
+ch will have values as ("A", "B", ... "J")
+
+
+demonstration of while lop
+
+
+break;
+break is used inside a loop and it will break 
+the nearest loop it is part of.
+
+
+
+"""
+
+# for ch in "ASHWIN":
+#     print("the character is ", ch)
+
+# cnt = 0
+# while cnt < 4:
+#     print("hello")
+#     cnt += 1
+
+
+# A program to search for 5 in range(0, 1000)
+
+# for x in range(0, 10):
+#     if x == 5:
+#         print("Found")
+#         break
+
+
+# for x in range(0, 10):
+#     if x < 5:
+#         print("will continue")
+#         continue
+#     print(x)
+
+
+# for x in range(0, 4):
+#     if x < 2:
+#         continue
+#     if x == 4:
+#         break
+#     print(x)
+
+
+
+
+# cnt = 0
+# while cnt < 10:
+#       if cnt < 5:
+#          cnt += 1
+#          continue
+#       if cnt == 7:
+#          break 
+#       print(cnt)
+#       cnt += 1
+
+
+# for i in range(10):
+#    cnt = 0
+#    while cnt < 10:
+#       print(cnt)
+#       cnt += 1
+#    break
+
+
+
+# cnt1 = 0
+# while cnt1 < 5:
+#     cnt2 = 0
+#     while cnt2 < 5:
+#          if cnt2 == 3:
+#             break
+#          if cnt2 == 0 or cnt2 == 1:
+#             cnt2 += 1
+#             continue
+#          print(cnt2)
+#          cnt2 += 1
+#     cnt1 += 1
+
+
+n = 3
+no = 1
+sum = 0
+while no <= n:
+   sum += no ** 3
+   no += 1
+
+print(sum)
+
+
