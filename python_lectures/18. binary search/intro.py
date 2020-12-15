7d93d705573929138f14a243a006f3081007a48b
shubham <joshi.cse17@gmail.com>
2020-12-15 23:29:10 +0530

    problemsAdded

--- python_lectures/18. binary search/intro.py
+++ python_lectures/18. binary search/intro.py
@@ -56,49 +56,111 @@
 
 
 
-def lowerbound(A, target) -> int:
-    n = len(A)
+# def lowerbound(A, target) -> int:
+#     n = len(A)
+
+#     left = 0
+#     right = n - 1
+    
+#     ans = -1
+
+#     while left <= right:
+#         mid = (left + right) >> 1 # or (left + right) // 2
+#         if A[mid] == target:
+#             ans = mid
+#             right = mid - 1
+#         elif A[mid] > target:
+#             right = mid - 1
+#         elif A[mid] < target:
+#             left = mid + 1
+    
+#     return ans
+
+# def upperbound(A, target) -> int:
+#     n = len(A)
+
+#     left = 0
+#     right = n - 1
+    
+#     ans = -1
+
+#     while left <= right:
+#         mid = (left + right) >> 1 # or (left + right) // 2
+#         if A[mid] == target:
+#             ans = mid
+#             left = mid + 1
+#         elif A[mid] > target:
+#             right = mid - 1
+#         elif A[mid] < target:
+#             left = mid + 1
+    
+#     return ans
+
+
+# if __name__ == "__main__":
+#     A = [1, 1, 1, 1, 2,2,2,2,2,2, 3, 3, 4, 5, 5]
+#     x = 2
+#     print(upperbound(A, x) - lowerbound(A, x) + 1)
+#     #2*log(n) = log(n)
+
+
+
+class Solution:
+    def findPeakElement(self, nums):
+        n = len(nums)
+        for i in range(n):
+            if i == 0:
+                if nums[i] > nums[i+1]:
+                    return i
+            elif i == n - 1:
+                if nums[i] > nums[i-1]:
+                    return i
+            else:
+                if nums[i] > nums[i-1] and nums[i] > nums[i+1]:
+                    return i
+
+
+sol = Solution()
+print(sol.findPeakElement([1,1,1,12,3]))
+
+
+# def solve(A):
+#     left = 0
+#     right = len(A) - 1
+#     while left <= right:
+#         mid = (left + right) >> 1
+#         if mid == 0 and A[mid + 1] < A[mid]:
+#             return mid
+#         if mid == len(A) - 1 and A[mid] > A[mid-1]:
+#             return mid
+        
+#         if A[mid] >= A[mid-1] and A[mid] > A[mid+1]:
+#             return mid
+#         elif A[mid+1] > A[mid]:
+#             left = mid + 1
+#         elif A[mid-1] > A[mid]:
+#             right = mid - 1
+        
+def lowerbound(A, target):
+    n = len(A) - 1
 
     left = 0
     right = n - 1
-    
     ans = -1
-
     while left <= right:
-        mid = (left + right) >> 1 # or (left + right) // 2
-        if A[mid] == target:
-            ans = mid
-            right = mid - 1
-        elif A[mid] > target:
+        mid = (left + right) >> 1
+        if A[mid] > target:
             right = mid - 1
         elif A[mid] < target:
             left = mid + 1
-    
-    return ans
-
-def upperbound(A, target) -> int:
-    n = len(A)
-
-    left = 0
-    right = n - 1
-    
-    ans = -1
-
-    while left <= right:
-        mid = (left + right) >> 1 # or (left + right) // 2
-        if A[mid] == target:
             ans = mid
-            left = mid + 1
-        elif A[mid] > target:
+        elif A[mid] == target:
+            ans = mid
             right = mid - 1
-        elif A[mid] < target:
-            left = mid + 1
-    
     return ans
 
 
 if __name__ == "__main__":
-    A = [1, 1, 1, 1, 2,2,2,2,2,2, 3, 3, 4, 5, 5]
-    x = 2
-    print(upperbound(A, x) - lowerbound(A, x) + 1)
-    #2*log(n) = log(n)
+    lowerbound([1,1,1,])
+
+
